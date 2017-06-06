f28d9a99bbf595e39b76401a383d9511bae6e824
Sergio <smartinsantos@gmail.com>
2017-06-06 10:03:50 -0500

    first commit

---
+++ app/routes/apiRouter.js
@@ -0,0 +1,7 @@
+const router = require('express').Router()
+// // routers declarations
+// const testRouter = require('./testRouter')
+
+// router.use('/test', testRouter)
+
+module.exports = router
